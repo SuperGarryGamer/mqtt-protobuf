f7e649dd8234288dc47985fab639123c48fd8275
SuperGarryGamer <melentyev.jay@gmail.com>
2024-10-28 22:52:32 +0100

    subscriber app "should work"

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
 import paho.mqtt.client as mqtt
 import numpy as np
+import config as cfg
+import weather_pb2
+import time
 
-    
+
+def main():
+    mqtt_client = mqtt.Client()
+    mqtt_client.on_connect = on_connect
+    mqtt_client.on_message = on_message
+
+    mqtt_client.connect(cfg.BROKER_IP, 1883, 60)
+    main_loop(mqtt_client)
+
+def main_loop(client: mqtt.Client):
+    client.loop_start()
+    for i in range(10):
+        client.publish(topic="temp", payload=fake_weather())
+        time.sleep(0.5)
+    client.loop_stop()
+
+def on_connect(client, userdata, flags, rc):
+    print(f"Connected to {cfg.BROKER_IP}, code {str(rc)}")
+
+def on_message(client, userdata, msg):
+    print(f"{msg.topic}: {str(msg.payload)}")
+
+def fake_weather():
+    weather = weather_pb2.Weather()
+    weather.timestamp = int(time.time())
+    weather.temp_centicelsius = int(np.random.normal(2000, 500))
+    weather.pres_hpa = int(np.random.normal(1013, 5))
+    weather.descriptor = np.random.randint(0, 7)
+    return weather.SerializeToString()
 
 
-
-
-def fake_val(mean=20, sd=5):
-    return np.random.normal(mean, sd)
+if __name__ == "__main__":
+    main()
--- subscriber.py
+++ subscriber.py
@@ -1,2 +1,27 @@
 import paho.mqtt.client as mqtt
+import config as cfg
 import weather_pb2
+import datetime
+
+def main():
+    mqtt_client = mqtt.Client()
+    mqtt_client.on_connect = on_connect
+    mqtt_client.on_message = on_message
+
+    mqtt_client.connect(cfg.BROKER_IP, 1883, 60)
+    mqtt_client.loop_forever()
+
+def on_connect(client, userdata, flags, rc):
+    print(f"Connected to {cfg.BROKER_IP}, code {str(rc)}")
+
+def on_message(client, userdata, msg):
+    if msg.topic == "temp":
+        pretty_print_protobuf(msg.content)
+
+def pretty_print_protobuf(raw: str):
+    weather = weather_pb2.Weather()
+    weather.ParseFromString(raw)
+    print(datetime.date.fromtimestamp(weather.timestamp).ctime())
+    print(f"Temperature: {weather.temp_centicelsius / 100}C")
+    print(f"Pressure: {weather.pres_hpa}hPa")
+    print(f"Conditions: {weather.desciptor}")
